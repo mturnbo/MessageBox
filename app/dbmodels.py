f2a1287441dd4eddaeafd4fdd43971645e86a53b
Marcus Turnbo <marcus@emarcus.com>
2025-11-01 15:46:05 -0500

    Added Message model and router

--- app/dbmodels.py
+++ app/dbmodels.py
@@ -16,3 +16,18 @@ class User(SQLModel, table=True):
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     last_seen: datetime = Field(default_factory=lambda: datetime.now(timezone.utc),
                                  sa_column_kwargs={"onupdate": lambda: datetime.now(timezone.utc)})
+
+
+class Message(SQLModel, table=True):
+    __tablename__ = "messages"
+    id: int = Field(primary_key=True)
+    sender_id: int = Field(foreign_key="users.id")
+    recipient_id: int = Field(foreign_key="users.id")
+    subject: str
+    body: str
+    sent_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    sender_address: str
+    read_at: datetime | None = None
+    reader_address: str | None = None
+    is_deleted_by_sender: bool = False
+    is_deleted_by_recipient: bool = False
---
+++ app/routers/messages.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from typing import List
+from sqlmodel import Session, select
+from app.dbmodels import Message
+from app.database import get_session
+
+router = APIRouter(prefix="/messages", tags=["messages"])
+
+@router.get("/{message_id}", response_model=Message)
+def get_messages_by_sender(message_id: int, session: Session = Depends(get_session)):
+    message = session.exec(select(Message).where(Message.id == message_id)).first()
+    if not message:
+        raise HTTPException(status_code=404, detail="Message not found")
+
+    return message
+
+
+@router.get("/sender/{user_id}", response_model=List[Message])
+def get_messages_by_sender(user_id: int, session: Session = Depends(get_session)):
+    messages = session.exec(select(Message).where(Message.sender_id == user_id)).all()
+    if not messages:
+        raise HTTPException(status_code=404, detail="Messages not found")
+
+    return messages
+
+
+@router.get("/recipient/{user_id}", response_model=List[Message])
+def get_messages_by_sender(user_id: int, session: Session = Depends(get_session)):
+    messages = session.exec(select(Message).where(Message.recipient_id == user_id)).all()
+    if not messages:
+        raise HTTPException(status_code=404, detail="Messages not found")
+
+    return messages
