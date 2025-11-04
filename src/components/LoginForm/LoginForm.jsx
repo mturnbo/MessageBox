ce9e8bc737001226cdced203d701e7e93ed94f13
Marcus Turnbo <marcus@emarcus.com>
2025-11-04 14:14:05 -0600

    Updated app icon

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
+import { API_BASE_URL } from '../../config.js';
 import './LoginForm.css';
 
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [token, setToken] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(''); // Clear previous errors
-    const LOGIN_URL = 'http://127.0.0.1:8000/auth';
+    const LOGIN_URL = `${API_BASE_URL}/auth`;
 
     try {
       const response = await fetch(LOGIN_URL, {
@@ -41,35 +41,41 @@ function LoginForm() {
 
   return (
     <>
-      <form className="loginform" onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    <form className="max-w-sm mx-auto loginform" onSubmit={handleSubmit}>
+      <div className="mb-5">
+        <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+          username</label>
+        <input type="tezt" id="username"
+               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+               required
+               value={username}
+               onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="mb-5">
+        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+          password</label>
+        <input type="password" id="password"
+               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+               required
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex items-start mb-5">
+        <div className="flex items-center h-5">
+          <input id="remember" type="checkbox" value=""
+                 className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div align="right">
-          <button type="submit">Log In</button>
-        </div>
-        <div align="center" className="tokenstr">{token}</div>
-      </form>
-    </>
-  )
+        <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
+          me</label>
+      </div>
+      <button type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
+      </button>
+    </form>
+</>
+)
 }
 
 export default LoginForm;
