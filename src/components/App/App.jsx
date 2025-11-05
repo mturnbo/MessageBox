dbc5c5df335132b17b4f9b478c1ee0c1baa033e6
Marcus Turnbo <marcus@emarcus.com>
2025-11-05 10:41:48 -0600

    Added AuthProvider

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,15 @@
+import { AuthProvider } from "../../AuthContext.jsx";
 import './App.css';
 import LoginForm from "../LoginForm/LoginForm.jsx";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <div className="App">
         <h2>Messaging APP</h2>
         <LoginForm />
       </div>
-    </>
+    </AuthProvider>
   )
 }
 
