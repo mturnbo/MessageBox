ec3a6203fbf9b67139a15089645f57f2f2be0c73
Marcus Turnbo <marcus@emarcus.com>
2026-05-12 22:58:55 -0500

    database update

--- app/database.py
+++ app/database.py
@@ -4,11 +4,11 @@ import os
 
 # Environment variables for database connection
 load_dotenv()
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")
+MYSQL_USER = os.getenv("MYSQL_USER", "")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
 MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "users_db")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "")
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
