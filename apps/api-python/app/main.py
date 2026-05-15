af071e7b66b746a257f8734e171d2be79cfeb689
Marcus Turnbo <marcus@emarcus.com>
2026-05-15 10:33:54 -0500

    Added health route for python api

--- apps/api-python/app/main.py
+++ apps/api-python/app/main.py
@@ -23,4 +23,4 @@ app.include_router(messages.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=4000)
