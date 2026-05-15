3d274c72a68a82e217554cbb50563274b47104da
Marcus Turnbo <marcus@emarcus.com>
2026-05-15 10:37:20 -0500

    api-python: updated port

--- apps/api-python/app/main.py
+++ apps/api-python/app/main.py
@@ -21,6 +21,9 @@ app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(messages.router)
 
+def main():
+  import uvicorn
+  uvicorn.run("app.main:app", host="127.0.0.1", port=4000)
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=4000)
+    main()
